6dec2b873ff62a5277c7b3502760624885c031f4
JzzzzSkr <huahuiie@outlook.com>
2024-03-03 12:26:26 +1100

    pause

--- src/pages/Layout/MusicPlayer/PlayerRight/index.jsx
+++ src/pages/Layout/MusicPlayer/PlayerRight/index.jsx
@@ -4,8 +4,31 @@ import useStore from "../../../../store";
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
 import PauseCircleIcon from "@mui/icons-material/PauseCircle";
+import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
 
 const PlayerRight = () => {
+  const { currentSong } = useStore();
+
+  // If there is no current song, display a simplified version of the player
+  if (!currentSong) {
+    return (
+      <div className="playerRightWrapper">
+        <ul className="button">
+          <li>
+            <div className="playerBtn">
+              <SkipPreviousIcon
+                style={{ marginRight: "1em", fontSize: "30px" }}
+              />
+              <PlayCircleFilledIcon style={{ fontSize: "30px" }} />
+              <SkipNextIcon style={{ marginLeft: "1em", fontSize: "30px" }} />
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  // If there is a current song, display the full player with progress bar
   return (
     <div className="playerRightWrapper">
       <ul className="button">
@@ -13,19 +36,20 @@ const PlayerRight = () => {
           <div className="playerBtn">
             <SkipPreviousIcon
               style={{ marginRight: "1em", fontSize: "30px" }}
-            ></SkipPreviousIcon>
-            <PauseCircleIcon style={{ fontSize: "30px" }}></PauseCircleIcon>
-            <SkipNextIcon
-              style={{ marginLeft: "1em", fontSize: "30px" }}
-            ></SkipNextIcon>
+            />
+            <PauseCircleIcon style={{ fontSize: "30px" }} />
+            <SkipNextIcon style={{ marginLeft: "1em", fontSize: "30px" }} />
           </div>
         </li>
         <li className="line">
           <span>0:00</span>
           <div className="progress-bar-background">
-            <div class="progress-bar-foreground" style={{ width: "50%" }}></div>
+            <div
+              className="progress-bar-foreground"
+              style={{ width: "50%" }}
+            ></div>
           </div>
-          <span>03:45</span>
+          <span>{currentSong.time}</span>
         </li>
       </ul>
     </div>
